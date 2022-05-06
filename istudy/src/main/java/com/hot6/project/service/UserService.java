5a58cad87172c8991eec5c8cb8eeb84c59708ba5
fnejd <97046334+fnejd@users.noreply.github.com>
2022-05-06 16:09:08 +0900

    no message

--- istudy/src/main/java/com/hot6/project/service/UserService.java
+++ istudy/src/main/java/com/hot6/project/service/UserService.java
@@ -5,17 +5,36 @@ import java.util.List;
 import com.hot6.project.vo.UserVO;
 
 public interface UserService {
-	public List<UserVO> MentorRecommend();
-	
-	//로그인
-	public UserVO loginCheck(UserVO vo);
-	
-	//아이디 찾기
-	public String findId(UserVO vo);
-	
-	//아이디, 이메일 일치하는 회원 있는지 확인
-	public int matchUser(UserVO vo);
-	
-	//임의로 생성한 비밀번호로 업데이트
-	public int pwdUpdate(String newPwd, UserVO vo);
+
+   public int userInsert(UserVO vo, String filePath);
+
+   // 아이디 중복 체크
+   public int idCheck(String id);
+
+   // 회원정보수정 폼 진입>로그인 정보 가져오기 (user 1명 선택)
+   public UserVO userSelect(String id);
+
+   // 회원정보 수정 - userEdit>userEditOk
+   public int userUpdate(UserVO vo, String filePath);
+
+   // 프로필 불러다 보기 (userView)
+   public UserVO userView(String id);
+
+   // 회원탈퇴
+   public int userDelete(String id);
+
+   public List<UserVO> MentorRecommend();
+   
+   //로그인
+   public UserVO loginCheck(UserVO vo);
+   public UserVO inquiryOfUserById(String id);
+
+   //아이디 찾기
+   public String findId(UserVO vo);
+   
+   //아이디, 이메일 일치하는 회원 있는지 확인
+   public int matchUser(UserVO vo);
+   
+   //임의로 생성한 비밀번호로 업데이트
+   public int pwdUpdate(String newPwd, UserVO vo);
 }
--- istudy/src/main/java/com/hot6/project/service/UserServiceImpl.java
+++ istudy/src/main/java/com/hot6/project/service/UserServiceImpl.java
@@ -10,35 +10,68 @@ import com.hot6.project.dao.UserDAO;
 import com.hot6.project.vo.UserVO;
 
 @Service
-public class UserServiceImpl implements UserService{
-	@Inject
-	UserDAO dao;
+public class UserServiceImpl implements UserService {
+   @Inject
+   UserDAO dao;
 
-	@Override
-	public List<UserVO> MentorRecommend() {
-		return dao.MentorRecommend();
-	}
+   @Override
+   public List<UserVO> MentorRecommend() {
+      return dao.MentorRecommend();
+   }
 
-	@Override
-	public UserVO loginCheck(UserVO vo) {
-		return dao.loginCheck(vo);
-	}
+   @Override
+   public int userInsert(UserVO vo, String filePath) {
+      return dao.userInsert(vo, filePath);
+   }
 
-	@Override
-	public String findId(UserVO vo) {
-		return dao.findId(vo);
-	}
+   @Override
+   public int idCheck(String id) {
+      return dao.idCheck(id);
+   }
 
-	@Override
-	public int matchUser(UserVO vo) {
-		return dao.matchUser(vo);
-	}
+   @Override
+   public UserVO loginCheck(UserVO vo) {
+      return dao.loginCheck(vo);
+   }
 
-	@Override
-	public int pwdUpdate(String newPwd, UserVO vo) {
-		return dao.pwdUpdate(newPwd, vo);
-	}
+   @Override
+   public UserVO inquiryOfUserById(String id) {
+      return dao.inquiryOfUserById(id);
+   }
 
-	
+   @Override
+   public UserVO userSelect(String id) {
+      return dao.userSelect(id);
+   }
+
+   @Override
+   public int userUpdate(UserVO vo, String filePath) {
+      return dao.userUpdate(vo, filePath);
+   }
+
+   @Override
+   public UserVO userView(String id) {
+      return dao.userView(id);
+   }
+
+   @Override
+   public int userDelete(String id) {
+      return dao.userDelete(id);
+   }
+   
+   @Override
+   public String findId(UserVO vo) {
+      return dao.findId(vo);
+   }
+
+   @Override
+   public int matchUser(UserVO vo) {
+      return dao.matchUser(vo);
+   }
+
+   @Override
+   public int pwdUpdate(String newPwd, UserVO vo) {
+      return dao.pwdUpdate(newPwd, vo);
+   }
 
 }
