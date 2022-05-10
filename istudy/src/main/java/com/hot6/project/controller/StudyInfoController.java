028a6ebfd3e34c30381195335ed7ac1d653499e9
Jayoung Kim <rlawkdud0000@gmail.com>
2022-05-10 11:33:25 +0900

    [FEAT] url 매핑 주소 변경

--- istudy/src/main/java/com/hot6/project/controller/StudyInfoController.java
+++ istudy/src/main/java/com/hot6/project/controller/StudyInfoController.java
@@ -4,28 +4,26 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hot6.project.service.StudyInfoService;
 import com.hot6.project.vo.PagingVO;
 import com.hot6.project.vo.StudyInfoVO;
 
-@RestController
-@RequestMapping("/istudy/study_home/")
+@Controller
 public class StudyInfoController {
 	@Inject
 	StudyInfoService service;
 	
 	// 스터디 정보 페이지
-	@GetMapping("/study_info/{study_num}") 
-	public ModelAndView studyInfo(@PathVariable int study_num, PagingVO vo) {
+	@GetMapping("/study/study_home") 
+	public ModelAndView studyHome(PagingVO vo) {
 		ModelAndView mav=new ModelAndView(); 
 		List<StudyInfoVO> langList=service.langAll(); // 카테고리 언어 출력
-		List<StudyInfoVO> list= service.studyInfo(study_num, vo);
+		List<StudyInfoVO> list= service.studyHome(vo);
 		if(list!=null) {
 			for(StudyInfoVO svo:list) {
 				List<String> langs=service.studyLang(svo.getStudy_num());
@@ -33,10 +31,9 @@ public class StudyInfoController {
 				svo.setLang_type_name_list(langs);
 			}
 		}
-		mav.addObject("studyInfo", service.studyInfo(study_num, vo));
 		mav.addObject("studyhome", list); 
 		mav.addObject("langList", langList);
-		mav.setViewName("/study/studyInfo"); 
+		mav.setViewName("/study/studyHome"); 
 		return mav; 
 	}
 }
