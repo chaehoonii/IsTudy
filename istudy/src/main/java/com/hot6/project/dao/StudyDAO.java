dbcd820a9ec199e0758bb67bef03fd8a1d99216e
Hyeyoung Kim <hyeyoung.dev@gmail.com>
2022-05-13 13:31:57 +0900

    [FEAT] 일정 drag&drop 사용한 수정 기능

--- istudy/src/main/java/com/hot6/project/dao/StudyDAO.java
+++ istudy/src/main/java/com/hot6/project/dao/StudyDAO.java
@@ -11,26 +11,13 @@ import com.hot6.project.vo.StudyVO;
 @Mapper
 @Repository
 public interface StudyDAO {
-	// 달력
-	public List<StudyVO> CalendarList(int study_num);
-
-	// 일정 등록
-	public int PlanInsert(StudyVO vo);
-
 	// 메인페이지 - 스터디 추천
 	public List<StudyVO> StudyRecommend(String user_id);
-	public List<String> StudyLangType(int study_num);
-	public List<String> StudyTag(int study_num);
-	// 일정 상세
-	public StudyVO CalendarDetail(int plan_num);
 
-	// 일정 삭제
-	public int CalendarDel(int plan_num);
-	
-	//일정 수정
-	public int CalendarEdit(StudyVO vo);
-	
-	//스터디 게시판 등록
+	public List<String> StudyLangType(int study_num);
+
+	public List<String> StudyTag(int study_num);
+
+	// 스터디 게시판 등록
 	public int StudyboardInsert(BoardVO vo);
-	
 }
--- istudy/src/main/java/com/hot6/project/service/StudyServiceImpl.java
+++ istudy/src/main/java/com/hot6/project/service/StudyServiceImpl.java
@@ -15,50 +15,29 @@ public class StudyServiceImpl implements StudyService{
 	@Inject
 	StudyDAO dao;
 
-	@Override
-	public List<StudyVO> CalendarList(int study_num) {
-		return dao.CalendarList(study_num);
-	}
-
-	@Override
-	public int PlanInsert(StudyVO vo) {
-		return dao.PlanInsert(vo);
-	}
 
 	@Override
 	public List<StudyVO> StudyRecommend(String user_id) {
 		return dao.StudyRecommend(user_id);
 	}
 
-	@Override
-	public StudyVO CalendarDetail(int plan_num) {
-		return dao.CalendarDetail(plan_num);
-	}
-
-	@Override
-	public int CalendarDel(int plan_num) {
-		return dao.CalendarDel(plan_num);
-	}
-
-	@Override
-	public int CalendarEdit(StudyVO vo) {
-		return dao.CalendarEdit(vo);
-	}
-	
 
 	@Override
 	public List<String> StudyLangType(int study_num) {
 		return dao.StudyLangType(study_num);
 	}
 
+
 	@Override
 	public List<String> StudyTag(int study_num) {
 		return dao.StudyTag(study_num);
 	}
 
+
 	@Override
 	public int StudyboardInsert(BoardVO vo) {
-		return dao.StudyboardInsert(vo);
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
