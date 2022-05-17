e742e322cfb15aa05800380456b508e6fbd8cfc1
fnejd <97046334+fnejd@users.noreply.github.com>
2022-05-17 15:41:37 +0900

    [FEAT] 마이페이지 글관리, 스터디 관리 css, js 파일 분리 #22

---
+++ istudy/src/main/webapp/js/mypage/mypage.js
@@ -0,0 +1,30 @@
+	$(function(){
+    		   
+    		   $("#searchText").keyup(function(){
+    			   
+    			   var val = $(this).val();
+    			   
+    			   if(val==""){
+    				   $(".eachContent").show();
+    				   
+    			   }else{			   
+    				   $(".eachContent").hide();
+    				   $(".eachContent:contains('"+val+"')").show();			   
+    			   } 			   
+    		   })		   
+    	   }); 
+    	   
+    	   
+    function ReplyDel(reply_num){
+    		 if(confirm("댓글을 삭제하시겠습니까?")){
+    			 location.href = '/board/replyDel?reply_num='+reply_num;
+    		 }
+    		 return false;
+    	 } 
+    	 
+     function BoardDel(board_num){
+    	     if(confirm("글을 삭제하시겠습니까?")){
+    	          location.href = "/board/boardDelete?board_num="+board_num;
+    	        }
+    	     return false;
+    	}
