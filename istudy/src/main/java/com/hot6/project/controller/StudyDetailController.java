05db8ae2f927c1df9b6f66632b0d484b00848b18
Jayoung Kim <rlawkdud0000@gmail.com>
2022-05-22 22:37:39 +0900

    [FEAT] 스터디 정보, 상세

--- istudy/src/main/java/com/hot6/project/controller/StudyDetailController.java
+++ istudy/src/main/java/com/hot6/project/controller/StudyDetailController.java
@@ -1,5 +1,7 @@
 package com.hot6.project.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,18 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hot6.project.service.StudyDetailService;
+import com.hot6.project.service.StudyInfoService;
+import com.hot6.project.vo.StudyVO;
 
 @RestController
 public class StudyDetailController{
 	@Inject
 	StudyDetailService service;
+	@Inject
+	StudyInfoService service2;
 	
 	// 스터디 상세 페이지
 	@GetMapping("/study/study_home/{study_num}")
 	public ModelAndView studyInfo(@PathVariable int study_num) {
 		ModelAndView mav=new ModelAndView();
+		StudyVO vo= service.studydetail(study_num);
+		List<String> langs=service2.studyLang(study_num);
+			StudyVO pvo=service2.studyPeople(study_num);
+				if(pvo!=null) {
+					vo.setIn_people(pvo.getIn_people());
+					vo.setRemain(pvo.getRemain());
+					vo.setMax(pvo.getMax());
+				}
+				System.out.println(study_num+"/"+pvo);
+				vo.setLang_list(langs);
+				vo.setTag_list(service2.StudyTag(study_num));
+			
 		//스터디 상세 페이지
-		mav.addObject("studydetail", service.studydetail(study_num));
+		mav.addObject("vo",vo);
+		/*28, 40줄 mav.addObject("studydetail", service.studydetail(study_num)); */
 		mav.addObject("studyuser", service.studyuser(study_num));
 		mav.setViewName("/study/studyDetail");
 		return mav;
--- istudy/src/main/java/com/hot6/project/service/StudyDetailService.java
+++ istudy/src/main/java/com/hot6/project/service/StudyDetailService.java
@@ -6,7 +6,7 @@ import com.hot6.project.vo.StudyVO;
 
 public interface StudyDetailService {
 	// 스터디글 상세보기
-		public List<StudyVO> studydetail(int study_num);
+		public StudyVO studydetail(int study_num);
 
 		public List<StudyVO> studyuser(int study_num);
 }
